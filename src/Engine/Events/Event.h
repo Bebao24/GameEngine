3ef0f723c41f443cf5c178ff5eed209fa240af5e
Bebao24 <baong2408@gmail.com>
2025-07-26 18:26:52 +0700

    Put events stuff inside a Engine namespace

--- src/Engine/Events/Event.h
+++ src/Engine/Events/Event.h
@@ -1,18 +1,21 @@
 #pragma once
 
-enum class EventType
+namespace Engine
 {
-    None = 0,
-    WindowClose,
-    KeyPressed,
-    MouseMoved
-};
+    enum class EventType
+    {
+        None = 0,
+        WindowClose,
+        KeyPressed,
+        MouseMoved
+    };
 
-class Event
-{
-public:
-    virtual EventType GetEventType() const = 0;
-    virtual const char* GetEventName() const = 0;
-    virtual ~Event() = default;
-};
+    class Event
+    {
+    public:
+        virtual EventType GetEventType() const = 0;
+        virtual const char* GetEventName() const = 0;
+        virtual ~Event() = default;
+    };
+}
 
